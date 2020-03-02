4ff8e0134f282211dc89ec1b8e6c3b73375df8e7
Camposm97 <camposm1205@gmail.com>
2020-03-02 11:11:24 -0500

    Finished HW4 p2

--- Homework/Campos_Homework_Set_4_p2.asm
+++ Homework/Campos_Homework_Set_4_p2.asm
@@ -18,33 +18,38 @@
 	move $a0 %n
 	syscall
 .end_macro
+
 .text
 main:
 	jal ask_For_Int
-	move $a0 $v0	# Move enterd integer to argument
+	move $a1 $v0	# a1 = v0 (entered integer)
 	jal compute_Sum
 
 terminate:
 	li $v0 10
 	syscall
 			
-ask_For_Int:
+ask_For_Int:	# Prompts user to enter an integer
 	print("Please enter an integer: ")
 	li $v0 5
 	syscall
 	jr $ra
 	
 compute_Sum:
-	print("sum = (")
-	move $t0 $a0		# t0 = a0 (entered integer)
-	loop_1:
+	print("sum = (")	# Print sum = with open parathesis
+	move $s0 $a1	# t0 = a1 (entered integer)
+	li $t0 0		# t0 = 0 (index)
+
+	loop_1:		
 		li $t1 2
-		slt 
-		prntInt($t1)
-		print(" + ")
-		j loop_1
-	print(")")
-compute_Sum_Done:
-	jr $ra
+		sllv $t1 $t1 $t0	# Shift t1 to the left t0 times
+		prntInt($t1)	# Print integer
+		addi $t0 $t0 1	# Increment t0 by 1
+		beq $t0 $s0 compute_Sum_Done	# If t0 == s0 then exit loop
+		print(" + ")	# Print plus sign
+	j loop_1
 	
+compute_Sum_Done:
+	print(")\n")	# Print close paranthesis
+	jr $ra
 	
