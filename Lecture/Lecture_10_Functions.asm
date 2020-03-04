75b7f34329ed97ec36f1f27d7356491274dc9285
Camposm97 <camposm1205@gmail.com>
2020-03-04 16:34:23 -0500

    Deleted Demo.asm

--- Lecture/Lecture_10_Functions.asm
+++ Lecture/Lecture_10_Functions.asm
@@ -15,15 +15,21 @@ main:
 	jal diff_Of_Sums
 	move $s1 $v0		# Move result in s1
 	
-	print("The difference of the sums is ")
-	
-	li $v0 1
-	move $a0 $s1
+	print("Diff_Of_Sums: ")
+	li $v0 1		# Print integer (the result)
+	move $a0 $s1		# a0 = s1
 	syscall
+	print("\n")		# Print new line
 	
-	print("\n")
-	
-	li $v0 10
+	print("Emit_Int: ")
+	li $a1 100		# Set upper bound
+	jal emit_Int		# Call emit_Int function
+	move $a0 $v0		# Move result into a0
+	li $v0 1		# Print result
+	syscall
+
+terminate:
+	li $v0 10	# End program
 	syscall
 
 diff_Of_Sums:	# Takes in arguments a0 - a3
@@ -31,3 +37,9 @@ diff_Of_Sums:	# Takes in arguments a0 - a3
 	add $a2 $a2 $a3	# a2 = a2 + a3
 	sub $v0 $a0 $a2	# v0 = a0 = a2
 	jr $ra
+	
+emit_Int:		# Has a a1 parameter for upper bound
+	li $v0 42	# Generate random number
+	syscall
+	move $v0 $a0
+	jr $ra		# Return to caller
