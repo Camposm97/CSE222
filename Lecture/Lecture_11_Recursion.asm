8f239880e21383ff4212a1c1afbc2115642b18b8
Camposm97 <camposm1205@gmail.com>
2020-03-04 18:03:52 -0500

    Need to fix recursion function

---
+++ Lecture/Lecture_11_Recursion.asm
@@ -0,0 +1,23 @@
+.text
+main:
+	li $a0 4		# a0 = 4
+	jal factorial	# Call factorial function
+	
+done:
+	li $v0 10		# End program
+	syscall
+
+factorial:				# Parameter (int n (a0))
+	addi $sp $sp -8		# Make room in stack pointer
+	sw $ra 0($sp)		# Save return address
+	sw $a0 4($sp)		# Save parameter
+	ble $a0 1 factorial_Arg_Less_Than_One	# If a0 <= 1 jump to factorial_Arg_Le_One
+	
+	li $v0
+	
+factorial_Arg_Le_One:
+	li $v0 1		# v0 = 1
+	lw $ra 0($sp)	# Load return address
+	addi $sp $sp 8	# Recover stack pointer
+	jr $ra			# Jump to caller
+	
