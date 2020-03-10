b3a7197070bf68b3d933f263f384d302892afcc7
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-03-10 16:46:46 -0400

    Working on reversing an array

--- Notes/Note_04_More_Functions.asm
+++ Notes/Note_04_More_Functions.asm
@@ -1,9 +1,36 @@
 .data
+arr1: .word 1, 2, 3
+arr1Size: .word 3
 .text
 main:
+	la $a0 arr1
+	lw $a1 arr1Size
+	jal functFindSum
 	
-
+	move $t0 $v0	# move the sum to t0
+	
+	li $v0 1
+	move $a0 $t0
+	syscall
+	
 done:
 	li $v0 10
 	syscall
+
+# Takes in two arguments where is a0 which should be an array and a1 should be the size of the array
+functFindSum:
+	move $s0 $a0
+	move $s1 $a1 
+	li $t0 0	# i (counter)
+	li $s2 0	# s2 = sum
 	
+findSumLoop:
+	beq $t0 $s1 findSumLoopDone	# If t0 == s1 then jump to findSumLoopDone
+	lw $t9 0($s0)	# Read an int from the array
+	add $s2 $s2 $t9	# sum = sum + t9
+	addi $t0 $t0 1	# Increment i
+	addi $s0 $s0 4	# Move to next cell
+	j findSumLoop
+findSumLoopDone:
+	move $v0 $s2	# v0 = sum
+	jr $ra
--- Notes/Note_05_Reverse_Array.asm
+++ Notes/Note_05_Reverse_Array.asm
@@ -1,18 +1,23 @@
 .data
 arr1: .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-arr1Size: .word 10
+size: .word 10
+arr2: .space 40
 .text
 main:
-	la $s0 arr1		# s0 = address of array
-	lw $s1 arr1Size	# s1 = size
-	move $t0 $s1	# Set t1 = s1
+	la $s0 arr1		# s0 = address of arr1
+	la $s1 arr2		# s1 = address of arr2
+	addi $s0 $s0 40	# Set s0 to the last cell (address) of the array
+	lw $s2 size		# s2 = size
+	move $t0 $s1		# Set t1 = s1 (i)
 	
 loop_Reverse:
-	beqz $t0 done
+	beqz $t0 done	# If t0 == 0, then jump to done
+	lw $t1 0($s0)	# Load number from array
+	sw $t1 0($s1)	# Save number to array2
 	
-	lw $t1 0($s0)
-	
-	
+	sub $t0 $t0 1	# t0 = t0 - 1 (decrement i)
+	addi $s0 $s0 4	# Move to next cell (forwards)
+	sub $s1 $s1 4	# Move to next cell (backwards)
 	j loop_Reverse
 
 done:
