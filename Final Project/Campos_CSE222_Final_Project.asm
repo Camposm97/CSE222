cfab0b752e84ca947bd9f45c30d967eaaea87b62
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-04-29 15:34:15 -0400

    Blessed program with clean code

--- Final Project/Campos_CSE222_Final_Project.asm
+++ Final Project/Campos_CSE222_Final_Project.asm
@@ -3,25 +3,28 @@
 strBinNum: .space 40		# 40 bytes = 40 chars
 strBinNumSize: .word 40		# Size of strBinNum
 strHeader: .asciiz "========================================[Campos_CSE222_Final_Project]========================================"
-strPrmptSignMagNum: .asciiz "32-Bit Sign/Magnitude Binary Number: "
+strPrmptSignMagNum: .asciiz "Input [32-Bit Sign/Magnitude Binary Number]: "
 strInvalidInput: .asciiz "Invalid input: "
 strRequirements: .asciiz "Fill out all 32 bits with spaces between for every 4 bits."
 strExample: .asciiz "Example: 0000 0000 0000 0000 0000 0000 0000 0000"
+strResult: .asciiz "Output [32-Bit Two's Complement Binary Number]: "
 newLine: .asciiz "\n"
-space: .asciiz " "
 .text
 main:
 	prntlnStr(strHeader)
 	jal askForBinNum	# Prompt user for signed binary number
-	la $a0 strBinNum	# Set up arguments
+	la $a0 strBinNum	# Load arguments
 	lw $a1 strBinNumSize
 	jal isValidBinNum	# Check if entered string is valid
 	beqz $v0 invalidInput
-	la $a0 strBinNum
+	la $a0 strBinNum	# Load arguments
 	lw $a1 strBinNumSize
-	jal convertToDecimal
+	jal convertBinStrToDecimal
+	move $s0 $v0	# Move result to s0
+	prntStr(strResult)
+	prntIntAsBin($s0)	# Print result as binary (two's complement)
 	j terminate
-invalidInput:	# String is invalid, re-prompt user
+invalidInput:		# String is invalid, re-prompt user
 	prntStr(strInvalidInput)
 	prntlnStr(strBinNum)
 	prntlnStr(strRequirements)
@@ -29,7 +32,8 @@ invalidInput:	# String is invalid, re-prompt user
 	prntStr(newLine)
 	j main
 terminate:	# Exit Program
-	exit()
+	li $v0 10
+	syscall
 
 # =====================================[FUNCTIONS BELOW]=====================================
 # Prompts user to enter binary number (sign/mag. format)
@@ -63,7 +67,7 @@ validBinLoop:
 	beqz $v0 doneValidBinLoop
 	
 	addi $t0 $t0 1	# Increment counter
-	addi $s0 $s0 1	# Next Character
+	addi $s0 $s0 1	# Next char
 	j validBinLoop
 doneValidBinLoop:
 	seq $v0 $t0 $s1	# v0 = 1 if valid binary number
@@ -72,53 +76,47 @@ doneValidBinLoop:
 
 # Takes a0 (binary string) and converts it to base 10 format
 # a1 = length of string
-convertToDecimal:
+convertBinStrToDecimal:
 	saveAddr()
 	move $s0 $a0	# Store string in s0
 	move $s1 $a1	# Store length of string in s1
-	#addi $s1 $s1 -1	# Decrement length of string by 1 (So we avoid the NUL char)
-	li $s2 0		# This will hold the sum for computing value of the binary number in decimal
-	li $s3 30		# Bit Counter
+	addi $s1 $s1 -1	# Decrement length of string by 1 (avoids the NUL char)
+	li $s2 0		# This will hold the RESULT for computing value of the binary numbers in decimal
+	li $s3 30		# Bit Counter (Used for shifitng powers of 2)
 	li $t0 0		# Counter
 	li $t1 0		# For storing read chars
 	li $t2 1		# Most significant bit (msb = -1 or 1)
 	li $t3 1		# t3 = 1 (For shifting)
+	li $t4 0		# For storing temp results
 
-	jal isNegNum
+	jal isNegNum	# Check if msb is 1
 	addi $s0 $s0 1	# Next char
 	addi $t0 $t0 1	# Increment counter
-	bnez $v0 isNegative
+	bnez $v0 isNegative	# If v0 == 1, then jump
 	j iterate
 isNegative:
 	li $t2 -1
-iterate:
+iterateLoop:
 	beq $t0 $s1 doneIterate
-	lb $t1 0($s0)	# Read char
-	move $a0 $t1	# Move char to a0
-	jal isSpaceChar	# Is space char?
-	bnez $v0 contIterate	# If true, then 
-	addi $t1 $t1 -48	# Parse bit char to integer
-	#sub $t4 $s1 $t0	# t4 = s1 - t0
-	sllv $t4 $t3 $s3	# t4 = t3 << s3
-	
-	prntInt($t1)	# Print parsed int
-	print(" * ")
-	prntlnInt($t4)	# Print power
-	
+	lb $t1 0($s0)		# Read char
+	move $a0 $t1		# Move char to a0
+	jal isSpaceChar		# Is space char?
+	bnez $v0 contIterate	# If true, then jump
+	# If false, then compute result of that bit
+	addi $t1 $t1 -48		# Parse bit char to integer
+	sllv $t4 $t3 $s3		# t4 = t3 << s3
 	mult $t1 $t4	# LO = t1 * t4
-	mflo $t4		# t4 = LO
-	add $s2 $s2 $t4	# sum = sum + t4
+	mflo $t4		# t4 = LO (temp result)
+	add $s2 $s2 $t4	# Add temp result to s2
 	addi $s3 $s3 -1	# Decrement bit counter
 contIterate:
 	addi $s0 $s0 1	# Next char
 	addi $t0 $t0 1	# Increment counter
-	j iterate
+	j iterateLoop
 doneIterate:	
-	prntStr(newLine)
-	mult $t2 $s2
-	mflo $s2
-	prntlnInt($s2)
-	move $v0 $s2
+	mult $t2 $s2	# LO = msb's result * s2
+	mflo $s2		# s2 = LO (final result)
+	move $v0 $s2	# Store final result in v0
 	loadAddr()
 	jr $ra
 
