8d21e42f3659e2a69e08286c3a703afd5de4aeb7
Michael Campos <39308094+Camposm97@users.noreply.github.com>
2020-05-13 14:20:43 -0400

    Doing another version of final project
    Using more logic, thus more efficient.

---
+++ Final Project/Campos_CSE222_Final_Project_v3.asm
@@ -0,0 +1,62 @@
+.data
+input: .space 32
+buffer: .space 4
+inputSize: .word 32
+stringInput: .asciiz "Input: "
+stringOutput: .asciiz "Output: "
+newLine: .asciiz "\n"
+
+.text
+main:
+	li $v0 4
+	la $a0 stringInput
+	syscall
+	
+	li $v0 8
+	la $a0 input
+	li $a1 33
+	syscall
+	
+	li $v0 4
+	la $a0 newLine
+	syscall
+	
+	la $s0 input	# s0 = input
+	lb $t0 0($s0)	# load first char
+	sub $t0 $t0 48	# Parse to int
+	andi $t0 $t0 1	# t0 = t0 AND 1
+	
+	beqz $t0 returnInput	# If t0 == '0', returnInput
+startConverting:	# Else start conversion to two's comp
+	li $v0 4
+	la $a0 stringOutput
+	syscall
+	j done
+returnInput:
+	li $v0 4
+	la $a0 stringOutput
+	syscall
+	jal displayResult
+	j done
+done:
+	li $v0 10
+	syscall
+	
+displayResult:	# Displays arrray of char in input
+	la $s0 input
+	lw $s1 inputSize
+	li $t0 0	# counter
+	li $a0 0	# for storing char from array
+displayLoop:
+	beq $t0 $s1 displayDone
+	lb $a0 0($s0)	# Load char
+	li $v0 11		# Print char
+	syscall
+	addi $t0 $t0 1
+	addi $s0 $s0 1
+	j displayLoop
+displayDone:
+	li $v0 4
+	la $a0 newLine
+	syscall
+	jr $ra
--- Homework/Campos_CSE222_HW8_p6.asm
+++ Homework/Campos_CSE222_HW8_p6.asm
@@ -37,19 +37,19 @@ save_To_X:
 
 # Returns value inside x
 load_X:
-	lw $v0 x
+	lw $v0 x	# v0 = x
 	jr $ra
 
 # v0 = a0 * a1
 multiply:
 	mult $a0 $a1
-	mflo $v0
+	mflo $v0	# v0 = product of a0 * a1
 	jr $ra
 
 # Returns result of v0 = 7 * x
 compute_7x:
-	sub $sp $sp 4
-	sw $ra 0($sp)
+	sub $sp $sp 4	# Make space in the stack pointer
+	sw $ra 0($sp)	# Save ra in the stack pointer
 	jal load_X
 	move $a0 $v0	# a0 = x
 	li $a1 7		# a1 = 7
